da022cf0178e1c70d1ab9deff310894c05083239
Sigmaalex <sigaalex@mail.ru>
2023-02-20 13:52:49 +0500

    task 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,45 @@
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+        task6();
+        task7();
+
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+        Scanner s = new Scanner(System.in);
+        int clientOs = s.nextInt();
+        if (clientOs == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOs == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+    }
+
+    public static void task2() {
+        System.out.println("Задача 2");
+        Scanner s = new Scanner(System.in);
+        int clientOs = s.nextInt();
+        int year = s.nextInt();
+        if (year <= 2015)
+            if (clientOs == 0) {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            } else if (clientOs == 1) {
+                System.out.println("Установите версию приложения для Android по ссылке");
+            } else {
+                if (clientOs == 0) {
+                    System.out.println("Установите версию приложения для iOS по ссылке");
+                } else if (clientOs == 1) {
+                    System.out.println("Установите версию приложения для Android по ссылке");
+                }
+            }
     }
 }
